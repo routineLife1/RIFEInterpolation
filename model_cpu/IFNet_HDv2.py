91f01aea9d743216954c70de87abc8312cd6023a
YiWeiHuang-stack <68835291+YiWeiHuang-stack@users.noreply.github.com>
2021-03-02 03:06:43 +0800

    Update IFNet_HDv2.py

--- model_cpu/IFNet_HDv2.py
+++ model_cpu/IFNet_HDv2.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-from mode_cpu.warplayer import warp
+from model_cpu.warplayer import warp
 
 device = torch.device("cpu")
 
